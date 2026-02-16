a0fb86c6d39cf273a1f4776e3093903d9d993396
waifal <waifalcode@gmail.com>
2026-02-16 17:06:34 +1300

    calculator now works!

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -15,7 +15,6 @@ import TailwindCSS from "/src/assets/css/output.css" with { type: "css" };
 
 // Utilities
 import BuildComponent from "./../../utils/BuildComponent.js";
-import formatCurrency from "../../utils/FormatCurrency.js";
 
 // Components
 import "./ExchangeRateSelector.js";
@@ -50,33 +49,121 @@ class Calculator extends HTMLElement {
 
         // Root
         this._root = this.attachShadow({ mode: "closed" });
-        template.component(this._root).then(() => this.#_render());
+        template.component(this._root).then(() => this.#_init());
 
         // Formatter
         this.format = (amount, locales = null, currency = null) => formatCurrency(amount);
     }
 
     get convertedCost() {
-        return (this.cost * this.exchangeRate);
+        const cost = Number(this.$cost.value);
+        const rate = Number(this.$exchangeRate.value);
+        return cost && rate ? cost * rate : 0;
     }
 
     get minimumPrice() {
-        return (2 * this.convertedCost);
+        return 2 * this.convertedCost;
     }
 
-    #_render() {
-        // Queries
-        this.$cost           = this._root.querySelector("#cost");
-        this.$exchangeRate   = this._root.querySelector("#exchangeRate");
-        this.$revenue        = this._root.querySelector("#estSale");
+    get revenue() {
+        return Number(this.$revenue.value);
+    }
 
+    get totalProfit() {
+        return this.revenue - this.convertedCost;
+    }
+
+    get revinvestment() {
+        return 0.50 * this.totalProfit;
+    }
+
+    get float() {
+        return 0.30 * this.totalProfit;
+    }
+
+    get personalProfit() {
+        return 0.20 * this.totalProfit;
+    }
+
+
+    #_init() {
+        // Queries
+        this.$cost              = this._root.querySelector("#cost");
+        this.$exchangeRate      = this._root.querySelector("#exchangeRate");
+        this.$revenue           = this._root.querySelector("#estSale");
+
+        this.#_render();
+    }
+
+    #_handleCalculateButton() {
+        const cost            = this._root.getElementById("cost");
+        const exchangeRate    = this._root.getElementById("exchangeRate");
+        const estSale         = this._root.getElementById("estSale");
+        this.$calculateButton = this._root.querySelector("button");
+
+        const handleMissingInputValue = (fields) => {
+            let hasMissing = false;
+            Object.entries(fields).forEach(([name, value]) => {
+                if (!value) {
+                    console.warn(`[Calculator Form] "${name}" is missing or invalid!`);
+                    hasMissing = true;
+                }
+            });
+            return hasMissing;
+        };
+
+        [cost, exchangeRate, estSale].forEach(el => {
+            el.onkeydown = e => {
+                const invalidKeys = ["E", "e", "-", "."];
+
+                if(invalidKeys.includes(e.key)) e.preventDefault();
+
+                e.key === "Enter" ? this.$calculateButton.click() : null;
+            }
+
+            el.onpaste = e => e.preventDefault();
+        });
+
+        this.$calculateButton.onclick = () => {
+            const [cost, exchangeRate, revenue] = [
+                this.$cost.value,
+                this.$exchangeRate.value,
+                this.$revenue.value
+            ];
+
+            const isMissing = handleMissingInputValue({ 
+                Cost: cost, 
+                "Exchange Rate": exchangeRate, 
+                Revenue: revenue 
+            });
+
+            if (isMissing) return;
+
+            const resultsTable = document.querySelector("app-results-table");
+
+            resultsTable.updateConvertedCost(this.convertedCost);
+            resultsTable.updateMinimumPrice(this.minimumPrice);
+            resultsTable.updateTotalProfit(this.totalProfit);
+            resultsTable.updateRevenue(this.revenue);
+            resultsTable.updateReinvestment(this.revinvestment);
+            resultsTable.updateFloat(this.float);
+            resultsTable.updatePersonalProfit(this.personalProfit);
+        };
+    }
+
+    #_handleExchangeRateSelection() {
         const exchangeRateSelect = this._root.querySelector("exchange-rate-selector");
-        
+            
         exchangeRateSelect.addEventListener("rate-change", e => {
             this.exchangeRate = parseFloat(e.detail.rate).toFixed(2);
 
             this.$exchangeRate.value = this.exchangeRate;
         });
+    }
+
+    #_render() {
+        this.#_handleExchangeRateSelection();
+        this.#_handleCalculateButton();
     }
 }
 
--- src/components/Calculator/ResultsTable.js
+++ src/components/Calculator/ResultsTable.js
@@ -15,6 +15,7 @@ import TailwindCSS from "/src/assets/css/output.css" with { type: "css" };
 
 // Utilities
 import BuildComponent from "../../utils/BuildComponent.js";
+import formatCurrency from "../../utils/FormatCurrency.js";
 
 const customCSS = `
     :host {
@@ -33,7 +34,71 @@ class ResultsTable extends HTMLElement {
 
         // Root
         this._root = this.attachShadow({ mode: "closed" });
-        template.component(this._root);
+        template.component(this._root).then(() => this.#_init());
+
+        // Formatter
+        this.format = (amount, locales = undefined, currency = undefined) => formatCurrency(amount, locales, currency);
+        
+        // Iterator
+        this.populateFields = (fields, value, isProfit = false) => {
+            if (!fields.forEach) fields = [fields];
+
+            fields.forEach(f => {
+                const amount = Number(value);
+                f.textContent = this.format(amount);
+
+                if (isProfit) {
+                    f.classList.remove("text-slate-400", "text-green-400", "text-red-500");
+
+                    if (amount > 0) {
+                        f.classList.add("text-green-400");
+                    } else if (amount < 0) {
+                        f.classList.add("text-red-500");
+                    } else {
+                        f.classList.add("text-slate-400");
+                    }
+                }
+            });
+        };
+    }
+
+    #_init() {
+        // Queries
+        this.convertedCost  = this._root.querySelectorAll("[data-converted-cost]");
+        this.minimumPrice   = this._root.querySelector("[data-minimum-price]");
+        this.revenue        = this._root.querySelectorAll("[data-revenue]");
+        this.totalProfit    = this._root.querySelectorAll("[data-total-profit]");
+        this.reinvestment   = this._root.querySelector("[data-reinvestment]");
+        this.float          = this._root.querySelector("[data-float]");
+        this.personalProfit = this._root.querySelector("[data-personal-profit]");
+    }
+
+    updateConvertedCost(value) {
+        this.populateFields(this.convertedCost, value);
+    }
+
+    updateMinimumPrice(value) {
+        this.minimumPrice.textContent = this.format(Number(value));
+    }
+
+    updateTotalProfit(value) {
+        this.populateFields(this.totalProfit, value, true);
+    }
+
+    updateRevenue(value) {
+        this.populateFields(this.revenue, value);
+    }
+
+    updateReinvestment(value) {
+        this.reinvestment.textContent = this.format(Number(value));
+    }
+
+    updateFloat(value) {
+        this.float.textContent = this.format(Number(value));
+    }
+
+    updatePersonalProfit(value) {
+        this.personalProfit.textContent = this.format(Number(value));
     }
 }
 
